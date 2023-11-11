41bca0d5854131a11abcfa56b0ad777f78d7227c
ianbuergis <ian@buergis.com>
2023-11-11 23:26:48 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='osuSwissBot',
+    version='0.1.0',
+    author='Ian Bürgis',
+
+    install_requires=[
+        'discord',
+        'pycord',
+        'ossapi',
+        'pillow',
+        'sqlalchemy',
+        'pymysql',
+        ],
+)
