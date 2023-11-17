c155a3ac6b3c283cfc836bbd612d070f1abce31c
ianbuergis <ian@buergis.com>
2023-11-17 17:59:31 +0100

    Removing unnecessary features

--- src/osuFeatures/scoreCalculator.py
+++ src/osuFeatures/scoreCalculator.py
@@ -1,10 +1,4 @@
-import os
-
-import ossapi
 import requests
-from peace_performance_python import Calculator, Beatmap
-from src.osuFeatures.mirrors.osuDirect import OsuDirect
-
 
 async def calculateScoreViaApi(mapId: int, *, s100: int = 0, s50: int = 0, miss: int = 0, mods: list = [], combo: int | None = None, rework: str = 'live') -> dict:
     link = 'https://pp-api.huismetbenen.nl/calculate-score'
@@ -23,20 +17,3 @@ async def calculateScoreViaApi(mapId: int, *, s100: int = 0, s50: int = 0, miss:
         request['combo'] = combo
 
     return requests.patch(link, json=request).json()
-
-async def calculateScoreViaRosu(beatmap: ossapi.Beatmap, *, s100: int = 0, s50: int = 0, miss: int = 0, mods: int, combo: int | None = None, mode: int = 0):
-    osuDirect = OsuDirect()
-    path = await osuDirect.getBeatmapFile(beatmap)
-    beatmap = Beatmap(path)
-
-    calculator = Calculator()
-    calculator.set_n100(s100)
-    calculator.set_n50(s50)
-    calculator.set_miss(miss)
-    calculator.set_mods(mods)
-    calculator.set_combo(combo)
-    calculator.set_mode(mode)
-    answer = calculator.calculate(beatmap)
-    del beatmap
-    os.remove(path)
-
