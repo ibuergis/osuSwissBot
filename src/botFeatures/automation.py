bb735441b55167f5d016f7622b4032b807616fd5
ibuergis <ian@buergis.com>
2023-12-19 00:14:01 +0100

    hotfix (again)

--- src/botFeatures/automation.py
+++ src/botFeatures/automation.py
@@ -1,3 +1,5 @@
+import time
+
 from discord.ext import tasks, commands
 from src.osuFeatures.osuHandler import OsuHandler
 from ossapi import GameMode
@@ -8,6 +10,8 @@ class Automation(commands.Cog):
 
     __osuHandler: OsuHandler
 
+    __usersUpdated: bool = False
+
     __getScoresLoop: int = 1
 
     def __init__(self, bot: commands.Bot, osuHandler: OsuHandler, checkPlays: bool = True):
@@ -17,7 +21,6 @@ class Automation(commands.Cog):
         if checkPlays:
             print('Recent scores are getting checked')
             self.updateUsers.start()
-            self.getScores.start()
         else:
             print('Recent scores are not getting checked')
 
@@ -25,6 +28,9 @@ class Automation(commands.Cog):
     async def updateUsers(self):
         print('updating users...')
         await self.__osuHandler.updateUsers()
+        if not self.__usersUpdated:
+            self.getScores.start()
+            self.__usersUpdated = True
         print('users are updated')
 
     @tasks.loop(minutes=2)
